b9312e7b50b5c8b23681a9f955ee542f53b5f2ec
Rama <gudrms2538@gmail.com>
2025-09-29 21:52:20 +0900

    add: add project, gitignore

---
+++ HonestHouse-Prototype/HonestHouse-Prototype/HonestHouse_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonestHouse_PrototypeApp.swift
+//  HonestHouse-Prototype
+//
+//  Created by Rama on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HonestHouse_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
