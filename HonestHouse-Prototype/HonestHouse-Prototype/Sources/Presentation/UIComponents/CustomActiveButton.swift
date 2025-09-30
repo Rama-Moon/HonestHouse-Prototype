620a9f1c4996e090a3e4792608c382dcec9632a5
dlguszoo <lhjivy1024@gmail.com>
2025-09-30 17:13:07 +0900

    [#11] Fix: enum Case, 컴포넌트 CustomActiveButton 재폴더링

--- HonestHouse-Prototype/HonestHouse-Prototype/Sources/Presentation/UIComponents/CustomActiveButton.swift
+++ HonestHouse-Prototype/HonestHouse-Prototype/Sources/Presentation/UIComponents/CustomActiveButton.swift
@@ -22,8 +22,8 @@ struct CustomActiveButton: View {
                 .frame(maxWidth: .infinity)
                 .padding()
                 .font(.system(size: 16, weight: .bold))
-                .background(isEnabled ? .hhcolor(color: .buttonBackground(.primary)) : .hhcolor(color: .buttonBackground(.disabled)))
-                .foregroundColor(isEnabled ? .hhcolor(color: .buttonText(.primary)) : .hhcolor(color: .buttonText(.disabled)))
+                .background(isEnabled ? Color.blue : Color.gray.opacity(0.3))
+                .foregroundColor(.white)
                 .cornerRadius(12)
         }
         .disabled(!isEnabled)
