2e99f35a15dd178f0b83b5784975334fd9e7046a
Rama <gudrms2538@gmail.com>
2025-09-30 15:22:57 +0900

    [#1] Feat: Onboarding 뷰, EV 측정 Preview 구현

---
+++ HonestHouse-Prototype/HonestHouse-Prototype/Camera.swift
@@ -0,0 +1,18 @@
+//
+//  CameraModel.swift
+//  HonestHouse-Prototype
+//
+//  Created by Rama on 9/30/25.
+//
+
+import SwiftUI
+
+struct Camera {
+    var company: String
+    var lens: String
+    var cameraBody: String
+    var maxOpenAperture: Float
+    var maxCloseAperture: Int = 22
+    var fastestShutterSpeed: Float
+    var slowestShutterSpeed: Float
+}
