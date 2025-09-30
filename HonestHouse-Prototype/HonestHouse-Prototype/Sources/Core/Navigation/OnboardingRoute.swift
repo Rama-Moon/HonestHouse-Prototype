45d631b895338e6700c013467fa9734358e3607f
BoMin <lbmleebm@gmail.com>
2025-10-01 01:24:03 +0900

    [#12] Feat: OnboardingRoute

---
+++ HonestHouse-Prototype/HonestHouse-Prototype/Sources/Core/Navigation/OnboardingRoute.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingRoute.swift
+//  HonestHouse-Prototype
+//
+//  Created by BoMin Lee on 10/1/25.
+//
+
+import Foundation
+
+enum OnboardingRoute: Hashable {
+    case cameraBody(Manufacturer)
+    case cameraLens(Manufacturer)
+}
