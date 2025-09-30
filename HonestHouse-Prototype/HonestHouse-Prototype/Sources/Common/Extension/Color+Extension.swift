0652808bfa607921c65256378ccc4033f89fcfb5
BoMin <lbmleebm@gmail.com>
2025-09-30 14:55:08 +0900

    [#6] Chore: Add Color Extension
    - Hex to Color

---
+++ HonestHouse-Prototype/HonestHouse-Prototype/Sources/Common/Extension/Color+Extension.swift
@@ -0,0 +1,48 @@
+//
+//  Color+Extension.swift
+//  HonestHouse-Prototype
+//
+//  Created by BoMin Lee on 9/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 6:
+            (r, g, b) = ((int >> 16) & 0xFF, (int >> 8) & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 1)
+        }
+        self.init(
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255
+        )
+    }
+    
+    func toHex() -> String? {
+        let uiColor = UIColor(self)
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        guard uiColor.getRed(&r, green: &g, blue: &b, alpha: &a) else {
+            return nil
+        }
+        
+        let rgb: Int = (Int)(r * 255) << 16 |
+        (Int)(g * 255) << 8 |
+        (Int)(b * 255) << 0
+        
+        return String(format: "%06X", rgb)
+    }
+    
+}
